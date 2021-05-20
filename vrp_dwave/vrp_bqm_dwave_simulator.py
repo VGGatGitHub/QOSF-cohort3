985651dca3d65b2b0effda3c7c6a7c6b6410ffa4
Aniket-235 <inffoaniket@gmail.com>
2021-05-20 14:03:58 +0530

    add vrp_dwave

---
+++ vrp_dwave/vrp_bqm_dwave_simulator.py
@@ -0,0 +1,37 @@
+# Load the packages that are required
+import qubo_formulation as qf
+
+from dwave.system import DWaveSampler, EmbeddingComposite
+from collections import defaultdict
+import dimod
+
+import dwave.inspector
+
+from dimod import BinaryQuadraticModel
+from dwave_qbsolv import QBSolv
+
+for n in range(3,10,1):
+    # for K in range(2,n):
+        K = n - 1
+        print(n,K)
+        initializer = qf.Initializer(n)
+        xc,yc,instance = initializer.generate_instance()
+        QUBO_formulator1 = qf.QUBO_formulator(instance, n, K)
+
+        Q, g, c, binary_cost = QUBO_formulator1.binary_representation()
+        Q_dict = defaultdict(float)
+
+        for i in range(n*K):
+            Q_dict[(i,i)] = Q[i][i]
+            for j in range(i+1,n*K):
+                Q_dict[(i,j)] = 2*Q[i][j]
+        
+        Q_dict_lin = defaultdict(float)
+        for i in range(n*K):
+            Q_dict_lin[(i)] = g[i]
+
+        offset = c
+        vartype = dimod.BINARY
+        BQM = BinaryQuadraticModel(Q_dict_lin,Q_dict, offset, vartype)
+        response = QBSolv().sample(BQM)
+        print(response)
